bd4bfa6477b0106a4daf2b057a053c4f53f22552
Jan Kučera <jankucera254@gmail.com>
2023-12-01 15:03:34 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,67 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val res = input.map {
+            return@map it.filter { c -> c.isDigit() }
+        }.map {"${it.first()}${it.last()}"
+        }.sumOf { it.toInt() }
+
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val conv = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9",
+        )
+
+        val res = input.parallelStream().mapToInt {
+            // replacing
+            var result = ""
+
+            var i = 0
+            label@while (i < it.length) {
+                if (it[i].isDigit()) {
+                    result += it[i]
+                    i++
+                    continue
+                }
+
+                for ( key in conv.keys ) {
+                    if (it.startsWith(key, i)) {
+                        val newI = i + key.length
+
+                        result += conv[key]
+
+                        i = newI
+                        continue@label
+                    }
+                }
+                i++
+            }
+
+            "${result.first()}${result.last()}".toInt()
+        }.sum();
+
+        return res
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("inputs/Day01.test.txt")
-    check(part1(testInput) == 1)
+    val testInput = readInput("inputs/Day01.test")
+    check(part1(testInput) == 142)
 
-    val input = readInput("inputs/Day01.input.txt")
+    val input = readInput("inputs/Day01.input")
     part1(input).println()
+
+    val testInput2 = readInput("inputs/Day01.test2")
+    check(part2(testInput2) == 281)
+
     part2(input).println()
 }
--- src/setup.sh
+++ src/setup.sh
@@ -17,8 +17,8 @@ mkdir "$INPUT_DIR" 2>/dev/null
 FILE="Day$1"
 TEMPLATE="Template.kt"
 
-INPUT_F="inputs/$FILE.input.txt"
-TEST_F="inputs/$FILE.test.txt"
+INPUT_F="inputs/$FILE.input"
+TEST_F="inputs/$FILE.test"
 
 
 sed -e "s,TEST,$TEST_F," -e "s,INPUT,$INPUT_F," "$TEMPLATE" > "$FILE.kt"
@@ -29,7 +29,7 @@ then
     fail
 fi
 
-touch "$TEST_F" "$INPUT_F"
+touch "$TEST_F.txt" "$INPUT_F.txt"
 STATUS=$?
 if [ $STATUS -ne 0 ]
 then
